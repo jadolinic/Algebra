7ce8c227c751c8a0a26c4a2f05060dbe5ebb7d59
jadolinic <jadolinic@gmail.com>
2023-11-26 13:13:08 +0100

    dodavanje opisa zadatka u komentar

--- zadaca5/script.js
+++ zadaca5/script.js
@@ -2,6 +2,9 @@
 
 // Zadatak 1.
 
+/* Imamo listu album i jedan objekt u toj listi. Napravi novi objekt i dodaj ga na u listu kao drugog
+člana liste (ali isto kao objekt) */
+
 const album = [
   {
     umjetnik: "Metallica",
@@ -17,6 +20,23 @@ const album = [
 ];
 
 // Zadatak 2.
+
+/* Idemo opet do Marka i Josipa i njihovog ITM-a.
+Ovaj put idemo koristiti objekte kako bi implementirali kalkulacije.
+Zapamtite ITM = masa/visina ** 2
+
+Za svakog od njih, kreiraj objekt sa vrijednostima : ime, težina, visina.
+Kreiraj calcITM metodu na svakom objektu koja će izračunati ITM (ista metoda na oba).
+
+Spremi ITM izračun u vrijednost objekta i vrati ga iz metode (return) ime
+
+logiraj u konzolu tko ima viši ITM, skupa sa imenom i ITM vrijednosti
+
+NPR: Josipov ITM (28.3) je veći od Markovog (23.9)!
+
+Test data: 
+Marko : 78 kg i 1.69 cm
+Josip : 92 kg i 1.95 cm */
 
 class Korisnici {
   constructor(ime, visina, tezina) {
